661c0279559a2b2fd1aaee45a87456eae8402480
Steven Sotelo <stevenbetancurt@hotmail.com>
2014-02-17 10:54:48 -0500

    Subida reto 2 finalizada Steven

--- src/P_2014_02/Reto_2_Steven.java
+++ src/P_2014_02/Reto_2_Steven.java
@@ -32,14 +32,9 @@ public class Reto_2_Steven
             String linea;
             String[] aux;
             
-            //Tabla hash
-            //Hashtable <Long,Integer> datos=new Hashtable<Long,Integer>();
-            /*long tiempoIProceso, tiempoFProceso, duracionProceso;
-
-            tiempoIProceso = System.currentTimeMillis();*/
-            // El trabajar con un HashMap redujo el tiempo de carga de 4024ms a 3524ms
             HashMap <Long,Integer> datos=new HashMap<Long,Integer>();
-            // Este ciclo carga los datos y los almacena en la tabla hash
+            
+            // Este ciclo carga los datos y los almacena en un hashmap
             // La clave del hash es el dato que encuentra
             // El valor almacenado en el hash es la cantidad de veces que se encuentra el número repetido            
             while((linea=lector.readLine())!=null)
@@ -61,71 +56,56 @@ public class Reto_2_Steven
             }
             lector.close();
             
-            /*tiempoFProceso = System.currentTimeMillis();
-            duracionProceso = tiempoFProceso - tiempoIProceso;
-            System.out.println("DuracionProceso="+duracionProceso+"ms \tEquivalente a "+duracionProceso/1000 + " s");
-            */
             Object[] keys=datos.keySet().toArray();
-            // Se puede realizar el ordenamiento mediante el API de java con Arrays.sort debido a Hashtable tiene una implementacion de comparable
+            // Se puede realizar el ordenamiento mediante el API de java con Arrays.sort debido a que hashmap tiene una implementacion de comparable
             // El algoritmo implementado para el ordenamiento es el mergesort por ser de tipo object
             Arrays.sort(keys);
             
-            
             //Lectura del archivo de borrar los datos
             lector=new BufferedReader(new FileReader(new File(file2)));
-            linea=null;
-            HashMap <Long,Integer> datosBorrar=new HashMap<Long,Integer>();
-            
+            linea=null;            
+            //Indice para recorrer las claves ya ordenadas
+            int pos=0;
             while((linea=lector.readLine())!=null)
             {
-                aux=linea.split("\t");
-                Long j=Long.parseLong(aux[0]);
-                Integer cantObj = datos.get(j);
-                Integer cantVal = Integer.parseInt(aux[1]);
-                if(cantObj==null)
-                    datosBorrar.put(j,cantVal);
-                else
-                    datosBorrar.put(j, cantObj + cantVal);
-            }
-            
-            Object[] keysBorrar=datosBorrar.keySet().toArray();
-            Arrays.sort(keysBorrar);
-            
-            for(Object k : keysBorrar)
-            {
-                Long num=Long.parseLong(k.toString());
-                Integer veces=datosBorrar.get(num);
-                int pos=Arrays.binarySearch(keys, k);
-                Integer cant=datos.get(keys[pos]);
-                if(veces==1 && cant==1)
-                    datos.remove(keys[pos]);
-                else if(veces==1 && cant > 1)
-                    datos.put(Long.parseLong(keys[pos].toString()), datos.get(keys[pos]) - 1);
-                else if(veces > 1)
+                // Se asigna los datos a borrar, esto depende si vienen separados por espacio o por tab
+                aux=linea.split("\t").length > 1 ? linea.split("\t") : linea.split(" ");                
+                // Tomo la cantidad de números que debo eliminar solamente, ya que siempre elimino los menores de la lista
+                Integer cantidad=Integer.parseInt(aux[1]);
+                //Creo un ciclo para eliminar la cantidad de datos que viene el registro 
+                while(cantidad > 0)                
                 {
-                    while(veces > 0 && pos >= 0)
+                    //Obtengo la cantidad de veces que tengo registrado el número
+                    Integer cDatos = datos.get(Long.parseLong(keys[pos].toString()));
+                    // Si la cantidad de veces que tengo registrado el numero es mayor a la cantidad de datos a borrar
+                    // Disminuyo su número de veces                  
+                    if(cDatos > cantidad)
                     {
-                        int temp= datos.get(keys[pos]);
-                        if(veces >= temp)
-                            datos.remove(keys[pos]);
-                        else
-                            datos.put(Long.parseLong(keys[pos].toString()), datos.get(keys[pos]) - veces);
-                        veces-=temp;
-                        pos-=1;
+                        datos.put(Long.parseLong(keys[pos].toString()), cDatos-cantidad);
+                        cantidad=0;
+                    }
+                    // Si por el contrario la cantidad de veces que tengo el número es menor o igual a los datos a borrar
+                    // Remuevo el dato, itero el indice en 1, asigno 0 a la clave para saber que ya salio de la lista
+                    // Disminuyo la cantidad de datos a borrar para la siguiente iteracion
+                    else
+                    {
+                        datos.remove(Long.parseLong(keys[pos].toString()));
+                        keys[pos++]=0;
+                        cantidad-=cDatos;
                     }
                 }
             }
-            for(Object k : keys)
+            lector.close();
+            for(Object k:keys)
             {
-                Integer can=datos.get(Long.parseLong(k.toString()));
-                if(can != null && can == 1 )
-                    System.out.println(k);
-                else if(can != null && can >1)
+                if(Long.parseLong(k.toString()) != 0)
                 {
-                    while(can > 0)
+                    for(int i=1;i<=datos.get(Long.parseLong(k.toString()));i++)
                         System.out.println(k);
                 }
             }
+            
+            // La ejecución tarda 1 min 48 sec
         }
         catch(IOException exIO)
         {
